fd4d92737628fff5b36dc75ebe163537eccb1a42
Merijn Testroote <merijn@studentvooralles.nl>
2020-03-04 15:20:16 +0100

    Update to 3.26.12

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [cljsjs/antd "3.26.6-0"]
+                 [cljsjs/antd "3.26.12-0"]
                  [cljsjs/moment "2.24.0-0"]])
